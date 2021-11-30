d5abf6a9420a5d421eb705941ad06e6eb36a3987
lonelyclown322 <60008218+lonelyclown322@users.noreply.github.com>
2021-11-30 15:18:56 +0500

    Update Pyatachok.java

--- classes/Pyatachok.java
+++ classes/Pyatachok.java
@@ -34,17 +34,12 @@ public class Pyatachok extends MainHero {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof MainHero)) return false;
-        MainHero mainHero = (MainHero) o;
-        return this.getAfraid() == mainHero.getAfraid() && Objects.equals(getName(), mainHero.getName())
-                && Objects.equals(getCurrentPosition(), mainHero.getCurrentPosition())
-                && getHome() == mainHero.getHome();
+        return super.equals(o);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getName(), getCurrentPosition(), getAfraid(), getHome());
+        return super.hashCode();
     }
 
     @Override
